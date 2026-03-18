58395c82c381784adcd4520d8af159c10e316be8
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-03-18 16:55:23 +0200

    Feature: query data using tanstack infinitequery

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import SearchPage from "./pages/Search";
+import SearchResultsPage from "./pages/Results";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Routes>
-      <Route element={<SearchPage />} path="/" />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route element={<SearchPage />} path="/" />
+        <Route element={<SearchResultsPage />} path="/results" />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
