e5ce584bb91f1abe42c739554d04f79a8f1399e3
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-03-24 23:43:15 +0200

    feature: reusable search input

--- src/features/userSearch/searchInput.tsx
+++ src/features/userSearch/searchInput.tsx
@@ -1,24 +1,24 @@
-import { Input } from "@heroui/input";
-import { Button } from "@heroui/button";
-import { useNavigate } from "react-router-dom";
-
 import { SearchIcon } from "@/components/icons";
 import { useAppStore } from "@/store/store";
-import { title } from "@/components/primitives";
+import { Button, Input } from "@heroui/react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchInput() {
-  const { searchQuery, setSearchQuery } = useAppStore();
   const navigate = useNavigate();
+  const { searchQuery, setSearchQuery } = useAppStore();
 
   function handleSearch() {
-    navigate(`/results?`);
+    if (searchQuery) {
+      const params = new URLSearchParams();
+
+      params.append("query", searchQuery);
+      navigate(`/search?${params.toString()}`);
+    }
   }
 
   return (
-    <div className="w-full h-full flex flex-col flex-grow place-content-center place-items-center gap-4">
-      <div className={`${title({ color: "violet" })}`}>GitSearch</div>
-
-      <div className="w-full max-w-2xl mb-50">
+    <>
+      <div className="w-full ">
         <Input
           aria-label="Search"
           classNames={{
@@ -39,10 +39,12 @@ export default function SearchInput() {
           placeholder="Search for a github user..."
           type="search"
           value={searchQuery}
-          onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-          onValueChange={(value) => setSearchQuery(value)}
+          onKeyDown={(event: KeyboardEvent) =>
+            event.key === "Enter" && handleSearch()
+          }
+          onValueChange={(value: string) => setSearchQuery(value.trim())}
         />
       </div>
-    </div>
+    </>
   );
 }
