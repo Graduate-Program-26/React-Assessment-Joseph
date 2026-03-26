1518fc85de9189a9fdc919cc206ebbbc3b2e87fc
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-03-26 15:29:42 +0200

    feature: breadcrumbs

---
+++ src/components/AppBreadcrumbs.tsx
@@ -0,0 +1,33 @@
+import { Breadcrumbs, BreadcrumbItem } from "@heroui/react";
+import { useLocation, Link } from "react-router-dom";
+
+export default function AppBreadcrumbs() {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter(Boolean);
+
+  if (location.pathname === "/") {
+    return null;
+  }
+
+  return (
+    <Breadcrumbs
+      variant="solid"
+      underline="active"
+      className="bg-linear-to-r from-violet-600 via-cyan-400 to-pink-400 w-fit rounded-xl mb-4 p-0.5"
+    >
+      <BreadcrumbItem>
+        <Link to="/">Home</Link>
+      </BreadcrumbItem>
+      {pathnames.map((segment, index) => {
+        const path = `/${pathnames.slice(0, index + 1).join("/")}`;
+        const isLastPath = index === pathnames.length - 1;
+
+        return (
+          <BreadcrumbItem key={path} isCurrent={isLastPath}>
+            {isLastPath ? segment : <Link to={path}>{segment}</Link>}
+          </BreadcrumbItem>
+        );
+      })}
+    </Breadcrumbs>
+  );
+}
