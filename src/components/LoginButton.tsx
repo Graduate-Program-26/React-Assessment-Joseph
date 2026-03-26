f22fa4614106703401b6fe5c58cc07b1a83c3658
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-03-26 15:32:44 +0200

    feature: show loading state when loging in

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,13 +1,16 @@
+import { useAppStore } from "@/store/store";
 import { Button } from "@heroui/button";
 import { useLocation } from "react-router-dom";
 
 export function LoginButton() {
   const location = useLocation();
+  const setIsAuthLoading = useAppStore((store) => store.setIsAuthLoading);
 
   function handleLogin(pathname: string) {
+    setIsAuthLoading(false)
     window.location.href = `api/auth/login?redirectTo=${encodeURIComponent(pathname)}`;
   }
-  
+
   return (
     <Button
       className=""
