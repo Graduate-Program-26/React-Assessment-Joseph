285a202bda548db7933ffa4c9e701721d7e9b619
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-03-26 15:32:18 +0200

    ux: remove footer from layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,19 +1,16 @@
 import { Outlet } from "react-router-dom";
 
 import { Navbar } from "@/components/navbar";
+import AppBreadcrumbs from "@/components/appBreadcrumbs";
 
 export default function DefaultLayout() {
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-4">
+      <AppBreadcrumbs />
         <Outlet />
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <p className="text-sm text-default-500">
-          &copy; {new Date().getFullYear()} GitSearch. All rights reserved.
-        </p>
-      </footer>
     </div>
   );
 }
